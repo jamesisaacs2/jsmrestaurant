c484131a7792f11a616c57976a9ff90fdd47300a
James Isaacs <jamesisaacs@Yeshua5782.local>
2022-02-13 00:17:32 -0700

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
+import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => (
+	<nav className="app__navbar">
+		<div className="app__navbar-logo">
+			<img src={images.gericht} alt="app logo" />
+		</div>
+	</nav>
+);
 
 export default Navbar;
